3aee773f946aab191afd12b4748b65cb00bda77f
Martin Facciuto <martin_facciuto@hotmail.com>
2022-07-16 02:34:04 -0300

    +public, +favicon

--- src/index.js
+++ src/index.js
@@ -5,13 +5,20 @@ import { graphqlHTTP } from 'express-graphql'
 import graphqlSchema from './graphqlSchema.js'
 import logger from './logger.js'
 import cors from 'cors'
+import path from 'path'
+import favicon from 'serve-favicon'
 
 const { APP_PORT } = process.env
+const __dirname = path.resolve()
+
+console.log(__dirname)
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.png')))
 
 app.use('/', graphqlHTTP({
 	graphiql: true,
