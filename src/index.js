d29c2409b152c76547e820feffc0115a72a4a8a8
Martin Facciuto <martin_facciuto@hotmail.com>
2022-07-16 02:25:30 -0300

    +cors

--- src/index.js
+++ src/index.js
@@ -4,9 +4,14 @@ import express from 'express'
 import { graphqlHTTP } from 'express-graphql'
 import graphqlSchema from './graphqlSchema.js'
 import logger from './logger.js'
+import cors from 'cors'
+
+const { APP_PORT } = process.env
 
 const app = express()
-const { APP_PORT } = process.env
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.use('/', graphqlHTTP({
 	graphiql: true,
