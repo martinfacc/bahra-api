1f5829de82908bd0ac01ed4bbf2fc9d4fbc5441c
martinfacciuto <martin_facciuto@hotmail.com>
2022-10-22 15:17:20 -0300

    [u]

--- src/logger.js
+++ src/logger.js
@@ -2,11 +2,11 @@ import dotenv from 'dotenv'
 dotenv.config()
 import log4js from 'log4js'
 
-const { LOG_LEVEL } = process.env
+const { LOG_LEVEL = 'all', LOG_FILENAME = 'logs.log' } = process.env
 
 log4js.configure({
-	appenders: { cheese: { type: 'file', filename: 'cheese.log' } },
-	categories: { default: { appenders: ['cheese'], level: LOG_LEVEL } }
+	appenders: { cheese: { type: 'file', filename: LOG_FILENAME } },
+	categories: { default: { appenders: ['cheese'], level: LOG_LEVEL } },
 })
 
 const logger = log4js.getLogger('logger')
